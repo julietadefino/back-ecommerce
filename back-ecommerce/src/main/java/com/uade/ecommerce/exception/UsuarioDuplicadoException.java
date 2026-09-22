06d0f4841934a43e6afd0e15044b8afb99fa9c9f
nachosc055 <nacho.sc07@gmail.com>
2026-09-22 17:27:37 -0300

    Update UsuarioDuplicadoException.java

--- back-ecommerce/src/main/java/com/uade/ecommerce/exception/UsuarioDuplicadoException.java
+++ back-ecommerce/src/main/java/com/uade/ecommerce/exception/UsuarioDuplicadoException.java
@@ -1,1 +1,8 @@
+package com.uade.ecommerce.exception;
 
+@ExceptionHandler(UsuarioDuplicadoException.class)
+    public ResponseEntity<ErrorRespuestaDTO> handleUsuarioDuplicado(UsuarioDuplicadoException ex) {
+        ErrorRespuestaDTO error = new ErrorRespuestaDTO("USUARIO_DUPLICADO", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+}
